8490d9775a42d1ae5bd99c0b87d40f3d1b72972d
Ajit Kumar <ajit62768@gmail.com>
2026-09-22 23:07:54 +0530

    Initial commit

---
+++ backend/app/core/config.py
@@ -0,0 +1,17 @@
+import os
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+JWT_ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
+JWT_ACCESS_TOKEN_EXPIRE_MINUTES = int(
+    os.getenv("JWT_ACCESS_TOKEN_EXPIRE_MINUTES", "30")
+)
+
+
+if not JWT_SECRET_KEY:
+    raise RuntimeError("JWT_SECRET_KEY is not configured")
